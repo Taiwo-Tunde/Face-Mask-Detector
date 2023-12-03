01e42b0fe53c9d3fb0c0cc033bd3ac47092d7d4d
Taiwo <taiwotunde32@gmail.com>
2023-12-03 10:52:45 +0100

    updated with yolov5 model

--- train_mask_detector_with_yoloV5.py
+++ train_mask_detector_with_yoloV5.py
@@ -1,6 +1,5 @@
- # import the necessary packages
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import MobileNetV2
+# import the necessary packages
+from tensorflow.keras.preprocessing.image  import ImageDataGenerator
 from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
@@ -8,7 +7,6 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.utils import to_categorical
@@ -19,6 +17,10 @@ from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np    
 import os
+
+# YOLOv5 specific imports
+from yolov5.models import YOLOv5
+from yolov5.utils import load_model_weights
 
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
@@ -39,13 +41,12 @@ labels = []
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
-    	img_path = os.path.join(path, img)
-    	image = load_img(img_path, target_size=(224, 224))
-    	image = img_to_array(image)
-    	image = preprocess_input(image)
-
-    	data.append(image)
-    	labels.append(category)
+        img_path = os.path.join(path, img)
+        image = load_img(img_path, target_size=(224, 224))
+        image = img_to_array(image)
+        image /= 255.0  # Normalize pixel values to [0, 1]
+        data.append(image)
+        labels.append(category)
 
 # perform one-hot encoding on the labels
 lb = LabelBinarizer()
@@ -56,55 +57,37 @@ data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
+    test_size=0.20, stratify=labels, random_state=42)
 
 # construct the training image generator for data augmentation
 aug = ImageDataGenerator(
-	rotation_range=20,
-	zoom_range=0.15,
-	width_shift_range=0.2,
-	height_shift_range=0.2,
-	shear_range=0.15,
-	horizontal_flip=True,
-	fill_mode="nearest")
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest")
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
-baseModel = MobileNetV2(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
+# Define the YOLOv5 model
+model = YOLOv5(input_size=(224, 224, 3), num_classes=2)
 
-# construct the head of the model that will be placed on top of the
-# the base model
-headModel = baseModel.output
-headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
-headModel = Flatten(name="flatten")(headModel)
-headModel = Dense(128, activation="relu")(headModel)
-headModel = Dropout(0.5)(headModel)
-headModel = Dense(2, activation="softmax")(headModel)
-
-# place the head FC model on top of the base model (this will become
- # the actual model we will train)
-model = Model(inputs=baseModel.input, outputs=headModel)
-
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
-for layer in baseModel.layers:
-	layer.trainable = False
+# Load the YOLOv5 weights (you can download the weights from the YOLOv5 repository)
+load_model_weights(model, 'yolov5s.pt')
 
 # compile our model
 print("[INFO] compiling model...")
-# opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer='Adam',
-	metrics=["accuracy"])
+    metrics=["accuracy"])
 
-# train the head of the network
-print("[INFO] training head...")
+# train the model
+print("[INFO] training YOLOv5 model...")
 H = model.fit(
-	aug.flow(trainX, trainY, batch_size=BS),
-	steps_per_epoch=len(trainX) // BS,
-	validation_data=(testX, testY),
-	validation_steps=len(testX) // BS,
-	epochs=EPOCHS)
+    aug.flow(trainX, trainY, batch_size=BS),
+    steps_per_epoch=len(trainX) // BS,
+    validation_data=(testX, testY),
+    validation_steps=len(testX) // BS,
+    epochs=EPOCHS)
 
 # make predictions on the testing set
 print("[INFO] evaluating network...")
@@ -116,11 +99,11 @@ predIdxs = np.argmax(predIdxs, axis=1)
 
 # show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
-	target_names=lb.classes_))
+    target_names=lb.classes_))
 
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
-model.save("mask_detector.model", save_format="h5")
+model.save("mask_detector_yolov5.model", save_format="h5")
 
 # plot the training loss and accuracy
 N = EPOCHS
